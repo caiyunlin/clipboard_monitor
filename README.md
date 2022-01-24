fc31870c32df9f86d88cf342f267d978e92493c1
cylin2000 <cylin2000@163.com>
2022-01-24 20:23:32 +0800

    initial update

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # clipboard_monitor
-A python clipboard monitor library
+
+Clipboard Monitor will monitor the clipboard changes and trigger register events.
+It's very easy to use, for details please refer below commands
+
+## Usages
+```python
+import clipboard_monitor 
+
+def print_text(text):
+	print("got text")
+	print(text)
+
+def print_files(files):
+	print("got files")
+	print(files)
+
+def print_image(image):
+	print("got image")
+	image.save("./test.png")
+
+clipboard_monitor.on_update(print)
+clipboard_monitor.on_text(print_text)
+clipboard_monitor.on_files(print_files)
+clipboard_monitor.on_image(print_image)
+
+clipboard_monitor.wait()
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="clipboard_monitor",
+    version="1.0.2",
+    description="Monitor Clipboard changes and trigger related actions",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cylin2000/clipboard_monitor/",
+    author="Calvin Cai",
+    author_email="cylin2000@163.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["clipboard_monitor"],
+    include_package_data=True,
+    install_requires=["pillow"],
+    entry_points={
+    },
+)
